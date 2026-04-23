14bdffd977671b032314d584fcf0e8daad6db044
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:15 +0530

    Added PrintName_example1 program

---
+++ src/basics/PrintName_example1.java
@@ -0,0 +1,11 @@
+public class PrintName_example1 {
+    public static void Printname(String name) {
+        System.out.println("Name is :" + name);
+
+    }
+
+    public static void main(String[] args) {
+        Printname("Darshan");
+        Printname("Aadhyata");
+    }
+}
