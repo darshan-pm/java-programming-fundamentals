3b90b8bf8fe8dbecb18069df7cb2f1cc7a779d42
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:16 +0530

    Added welcomeMessage program

---
+++ src/basics/welcomeMessage.java
@@ -0,0 +1,11 @@
+public class welcomeMessage {
+    public static void myMethod() {
+        System.out.println("Welcome to Java");
+
+    }
+
+    public static void main(String[] arsgs) {
+        myMethod();
+    }
+
+}
