b6c3520ed63d39b56f0d6a545a492c9995304ebc
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:19 +0530

    Added StringBuffer_example3 program

---
+++ src/strings/StringBuffer_example3.java
@@ -0,0 +1,8 @@
+public class StringBuffer_example3 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Darshan");
+        sb.deleteCharAt(4);
+        System.out.println(sb);
+    }
+
+}
