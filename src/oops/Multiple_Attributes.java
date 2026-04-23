3f0b9ddd41b0f2be1d598859845a88f2deb53e30
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:18 +0530

    Added Multiple_Attributes program

---
+++ src/oops/Multiple_Attributes.java
@@ -0,0 +1,13 @@
+public class Multiple_Attributes {
+    String name = "Darshan";
+    String surname = "Odeya";
+    int age = 18;
+
+    public static void main(String[] args) {
+        Multiple_Attributes myObj = new Multiple_Attributes();
+        System.out.println(myObj.name + " " + myObj.surname);
+        System.out.println(myObj.age);
+
+    }
+
+}
