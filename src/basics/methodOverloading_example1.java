d2afec657fc1b1b853550a9664150843cabaedcd
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:15 +0530

    Added methodOverloading_example1 program

---
+++ src/basics/methodOverloading_example1.java
@@ -0,0 +1,31 @@
+class Calculator {
+
+    public int add(int n1, int n2, int n3) {
+        return n1 + n2 + n3;
+    }
+
+    public int add(int n1, int n2) {
+
+        return n1 + n2;
+    }
+
+    public double add(double n1, double n2, double n3) {
+        return n1 + n2 + n3;
+    }
+}
+
+public class methodOverloading_example1 {
+    public static void main(String[] args) {
+        Calculator obj = new Calculator();
+
+        int r1 = obj.add(3, 4, 6);
+        System.out.println(r1);
+
+        int r2 = obj.add(5, 9);
+        System.out.println(r2);
+
+        double r3 = obj.add(4, 5, 6);
+        System.out.println(r3);
+
+    }
+}
