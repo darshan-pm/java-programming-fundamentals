4016c069651f21e8d0d442edc6d6cdbc7f5aa78b
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:19 +0530

    Added StringBuffer_example2 program

---
+++ src/strings/StringBuffer_example2.java
@@ -0,0 +1,8 @@
+public class StringBuffer_example2 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Darshan");
+        sb.append(" PM");
+        System.out.println(sb);
+    }
+
+}
