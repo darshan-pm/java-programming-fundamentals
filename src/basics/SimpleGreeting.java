67ba5cd912bacfd138875d96304d7deae2b2de8c
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:15 +0530

    Added SimpleGreeting program

---
+++ src/basics/SimpleGreeting.java
@@ -0,0 +1,15 @@
+public class SimpleGreeting {
+    public static void sayHello() {
+        System.out.println("Hello Darshan");
+
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+        sayHello();
+        sayHello();
+        sayHello();
+        sayHello();
+    }
+
+}
