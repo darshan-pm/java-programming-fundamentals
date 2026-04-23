74129bbd375db23c377d8770a74ec01b936cd6d7
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:13 +0530

    Added array_example4 program

---
+++ src/arrays/array_example4.java
@@ -0,0 +1,19 @@
+public class array_example4 {
+    public static void main(String[] args) {
+        int num[] = new int[10];
+        num[0] = 20;
+        num[1] = 18;
+        num[2] = 16;
+        num[3] = 14;
+        num[4] = 12;
+        num[5] = 10;
+        num[6] = 8;
+        num[7] = 6;
+        num[8] = 4;
+        num[9] = 2;
+        for (int i = 0; i <= 9; i++) {
+            System.out.println(num[i]);
+        }
+    }
+
+}
