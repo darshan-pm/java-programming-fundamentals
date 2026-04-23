c0ea160072b54794f14da6811b79bd380c71381f
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:15 +0530

    Added integir program

---
+++ src/basics/integir.java
@@ -0,0 +1,10 @@
+public class integir {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("The value of a is:" + a);
+        byte b = 29;
+        System.out.println("The value of b is:" + b);
+        short s = 30000;
+        System.out.println("The value of s is:" + s);
+    }
+}
