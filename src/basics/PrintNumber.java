061a8de7899d14d000e226e10fdaaace55c26739
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:15 +0530

    Added PrintNumber program

---
+++ src/basics/PrintNumber.java
@@ -0,0 +1,14 @@
+public class PrintNumber {
+    public static void PrintNumber(int number) {
+        System.out.println(number);
+
+    }
+
+    public static void main(String[] args) {
+        PrintNumber(98);
+        PrintNumber(56);
+        PrintNumber(90);
+
+    }
+
+}
