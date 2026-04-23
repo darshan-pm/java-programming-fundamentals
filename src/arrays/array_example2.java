48531453ad83c0d0aaafce039efe161499a926d3
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:13 +0530

    Added array_example2 program

---
+++ src/arrays/array_example2.java
@@ -0,0 +1,17 @@
+public class array_example2 {
+    public static void main(String[] args) {
+        int num[] = new int[4];
+        num[0] = 5;
+        num[1] = 8;
+        num[2] = 9;
+        num[3] = 1;
+
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+
+    }
+
+}
+
