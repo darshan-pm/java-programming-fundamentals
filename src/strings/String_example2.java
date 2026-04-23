ef82449aeec33446dbd21f5466fc417d69e0e109
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:20 +0530

    Added String_example2 program

---
+++ src/strings/String_example2.java
@@ -0,0 +1,9 @@
+public class String_example2 {
+    public static void main(String[] args) {
+        String name = "Darshan";
+        System.out.println("Hello " + name);
+        System.out.println(name.hashCode());
+        System.out.println(name.charAt(4));
+    }
+
+}
