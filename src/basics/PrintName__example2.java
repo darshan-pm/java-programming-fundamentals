3396fd99462c3859a8ef14280cc0c1979161f859
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:15 +0530

    Added PrintName__example2 program

---
+++ src/basics/PrintName__example2.java
@@ -0,0 +1,12 @@
+public class PrintName__example2 {
+    public static void Printname(String name) {
+        System.out.println("Name is :" + name);
+
+    }
+
+    public static void main(String[] args) {
+        Printname("Victus");
+        Printname("Omen");
+    }
+
+}
