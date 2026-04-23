b0c62fd4319f77aeb2931df83aa3caaf7cb7a677
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:17 +0530

    Added for_loop3 program

---
+++ src/loops/for_loop3.java
@@ -0,0 +1,11 @@
+public class for_loop3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            System.out.println("Day " + i);
+            for (int j = 1; j <= 9; j++) {
+
+            }
+        }
+    }
+
+}
