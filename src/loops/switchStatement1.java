f8bf6595d45cef10ccec474f2f2819432de97253
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:17 +0530

    Added switchStatement1 program

---
+++ src/loops/switchStatement1.java
@@ -0,0 +1,27 @@
+
+public class switchStatement1 {
+    public static void main(String[] args) {
+        int n = 4;
+        switch (n) {
+            case 1:
+                System.out.println("Darshan");
+                break;
+            case 2:
+                System.out.println("Pranav");
+                break;
+            case 3:
+                System.out.println("Aakash");
+                break;
+            case 4:
+                System.out.println("Koustub");
+                break;
+            case 5:
+                System.out.println("Dhanush");
+                break;
+            default:
+                System.out.println("Enter valid number:");
+
+        }
+    }
+
+}
