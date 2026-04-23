e388cd912a6e4f3b9b0f93ee9777fdd63534b2ba
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:15 +0530

    Added SimpleAddition program

---
+++ src/basics/SimpleAddition.java
@@ -0,0 +1,12 @@
+public class SimpleAddition {
+    public static int add(int a, int b) {
+        return a + b;
+
+    }
+
+    public static void main(String[] args) {
+        int result = add(6, 7);
+        System.out.println(result);
+
+    }
+}
