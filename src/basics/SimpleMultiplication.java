e70dcd68f133bf3a52dd1b08a30e47742dcb31ad
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:16 +0530

    Added SimpleMultiplication program

---
+++ src/basics/SimpleMultiplication.java
@@ -0,0 +1,12 @@
+public class SimpleMultiplication {
+    public static int Multiply(int a, int b) {
+        return a * b;
+
+    }
+
+    public static void main(String[] args) {
+        int result = Multiply(7, 6);
+        System.out.println(result);
+    }
+
+}
