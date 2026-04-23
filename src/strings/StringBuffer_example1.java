b97bc4c3293351aa6a0d068bfc182288f09fe2b9
Darshan <darshan200713@gmail.com>
2026-04-23 22:20:19 +0530

    Added StringBuffer_example1 program

---
+++ src/strings/StringBuffer_example1.java
@@ -0,0 +1,7 @@
+public class StringBuffer_example1 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        System.out.println(sb.capacity());
+    }
+
+}
