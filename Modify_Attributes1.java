0807bb850716e72c2583e1b9cff4c2def251be11
Darshan PM <darshan200713@gmail.com>
2026-05-05 23:43:51 +0530

    Delete Modify_Attributes1.java

--- Modify_Attributes1.java
+++
@@ -1,9 +0,0 @@
-public class Modify_Attributes1 {
-    int x = 10;
-
-    public static void main(String[] args) {
-        Modify_Attributes1 myObj = new Modify_Attributes1();
-        myObj.x = 5;
-        System.out.println(myObj.x);
-    }
-}
